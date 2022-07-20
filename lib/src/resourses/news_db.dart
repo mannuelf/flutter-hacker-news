754d8d974bd34b707387f59dc16be6d42b8ed1cc
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 15:23:05 +0200

    database provider

---
+++ lib/src/resourses/news_db.dart
@@ -0,0 +1,12 @@
+import 'package:newz/src/models/item.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:async';
+import '../models/item.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {}
+}
