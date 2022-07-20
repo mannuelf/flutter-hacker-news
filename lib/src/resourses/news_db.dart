b44f82b4f49b07f3795bdf6cea74f4fa148a6751
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 15:36:03 +0200

    SQL to create Items table

--- lib/src/resourses/news_db.dart
+++ lib/src/resourses/news_db.dart
@@ -3,15 +3,36 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:path/path.dart';
 import '../models/item.dart';
 
 class NewsDbProvider {
-  Database db;
+  late Database db;
+  String sqlCreateItemsTable = """
+    CREATE TABLE Items
+        (
+          id INTEGER PRIMARY KEY,
+          type TEXT,
+          by TEXT,
+          time TEXT,
+          parent INTEGER,
+          parent INTEGER,
+          kids BLOB,
+          dead INTEGER,
+          deleted INTGER,
+          url TEXT,
+          score INTEGER,
+          title TEXT,
+          descendants INTEGER
+        )
+  """;
 
   init() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
     final path = join(documentsDirectory.path, 'items.db');
-    db = await openDatabase(path,
-        version: 1, onCreate: (Database newDb, int version) {});
+    db = await openDatabase(path, version: 1,
+        onCreate: (Database newDb, int version) {
+      newDb.execute(sqlCreateItemsTable);
+    });
   }
 }
