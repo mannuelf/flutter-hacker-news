23f741f4cf42ad75e062e3a89fd781c976e2b578
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 12:00:55 +0200

    creates ItemModel to represent the API data

---
+++ lib/src/models/item.dart
@@ -0,0 +1,15 @@
+class ItemModel {
+  final int id = 0;
+  final bool deleted = false;
+  final String type = '';
+  final String by = '';
+  final int time = 0;
+  final String text = '';
+  final bool dead = false;
+  final int parent = 0;
+  final List<dynamic> kids = List.empty();
+  final String url = '';
+  final int score = 0;
+  final String title = '';
+  final int descendants = 0;
+}
