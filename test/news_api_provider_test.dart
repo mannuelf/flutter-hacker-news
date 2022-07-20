2f98c2e7b3df59cd821b37003a3483d0308d90a6
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 12:53:15 +0200

    completed test for an async request

--- test/news_api_provider_test.dart
+++ test/news_api_provider_test.dart
@@ -5,11 +5,15 @@ import 'package:http/testing.dart';
 import 'package:newz/src/resourses/news_api_provider.dart';
 
 void main() {
-  test('FetchTopIds returns a list of ids', () {
+  test('FetchTopIds returns a list of ids', () async {
     final newsApi = NewsApiProvider();
 
     newsApi.client = MockClient((request) async {
       return Response(json.encode([1, 2, 3, 4]), 200);
     });
+
+    final ids = await newsApi.fetchTopIds();
+
+    expect(ids, [1, 2, 3, 4]);
   });
 }
