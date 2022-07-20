5059489ccb7b4283d5e1a0a8eb1ecbd03448f01f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 16:10:43 +0200

    fetch all top ids

--- lib/src/resourses/repository.dart
+++ lib/src/resourses/repository.dart
@@ -5,11 +5,12 @@ import 'news_api_provider.dart';
 import 'news_api_provider.dart';
 import '../models/item.dart';
 
+// proxy to db, calls providers, app should not call providers directly
 class Repository {
   NewsDbProvider dbProvider = NewsDbProvider();
   NewsApiProvider apiProvider = NewsApiProvider();
 
-  fetchTopIds() {}
-
-  fetchItem() {}
+  fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
 }
