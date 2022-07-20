a4a1511d39a87f0a0d76f15a44aa1d101faa922b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 12:13:43 +0200

    Fields can't be initialized in the constructor if they are final and were already initialized at their declaration. Try removing one of the initializations

--- lib/src/models/item.dart
+++ lib/src/models/item.dart
@@ -1,20 +1,20 @@
 class ItemModel {
-  final int id = 0;
-  final bool deleted = false;
-  final String type = '';
-  final String by = '';
-  final int time = 0;
-  final String text = '';
-  final bool dead = false;
-  final int parent = 0;
-  final List<dynamic> kids = List.empty();
-  final String url = '';
-  final int score = 0;
-  final String title = '';
-  final int descendants = 0;
+  final int id;
+  final bool deleted;
+  final String type;
+  final String by;
+  final int time;
+  final String text;
+  final bool dead;
+  final int parent;
+  final List<dynamic> kids;
+  final String url;
+  final int score;
+  final String title;
+  final int descendants;
 
   // Receives a big blob of parsed json data
-  ItemModel.fromJson(Map<String dynamic> parsedJson)
+  ItemModel.fromJson(Map<String, dynamic> parsedJson)
       : id = parsedJson['id'],
         deleted = parsedJson['deleted'],
         type = parsedJson['type'],
