16ffa1891feeb788125d2b8a976a46aaf44734d3
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-20 12:38:50 +0200

    building tests

---
+++ test/news_api_provider_test.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:newz/src/resourses/news_api_provider.dart';
+
+void main() {
+  test('FetchTopIds returns a list of ids', () {
+    // setup test case
+
+    // expectation
+  });
+}
