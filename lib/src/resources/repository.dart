f7890be0d47e1377e46def8a677049e1e555c6db
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-21 11:12:59 +0200

    adds List of Cache and Source, makes repository class reusable

--- lib/src/resources/repository.dart
+++ lib/src/resources/repository.dart
@@ -6,8 +6,8 @@ import 'news_db_provider.dart';
 
 // proxy to db, calls providers, app should not call providers directly
 class Repository {
-  NewsDbProvider dbProvider = NewsDbProvider();
-  NewsApiProvider apiProvider = NewsApiProvider();
+  List<Source> sources = <Source>[NewsApiProvider(), NewsApiProvider()];
+  List<Cache> caches = <Cache>[NewsDbProvider()];
 
   Future<List> fetchTopIds() {
     return apiProvider.fetchTopIds();
