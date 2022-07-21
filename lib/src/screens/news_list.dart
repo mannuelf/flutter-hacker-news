3eb707492d306dd9ed23123672fa6b52ef9e0d18
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-21 12:52:48 +0200

    use the bloc data in teh new lsit

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,17 +1,24 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+
+import '../blocs/stories_bloc.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   const NewsList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // thing.of(context) gives us access to data up n down the widget tree
+    // we get access to the stream and get to use the data
+    final bloc = StoriesProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('TOP NEWS'),
       ),
-      body: Text('show list'),
+      body: buildList(bloc),
     );
   }
+
+  Widget buildList(StoriesBloc bloc) {}
 }
