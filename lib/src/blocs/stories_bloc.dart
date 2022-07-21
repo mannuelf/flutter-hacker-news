1f3123c9dd69e1a432b035945bb289792f6ea284
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-21 14:45:14 +0200

    public getter for transformed stream, our cacheed items

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -9,12 +9,23 @@ class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>(); // Observable of RXDart
   final _items = BehaviorSubject<int>();
+  // instance var that will hold the transformed streams
+  Stream<Map<int, Future<ItemModel>>> items;
 
   // Getter to Streams
   // https://stackoverflow.com/questions/59336428/observable-is-deprecated-in-rxdart-0-23-1
   Stream<List<int>> get topIds => _topIds.stream;
 
-  Function(int) get fetchItem => _items.sink.add;
+  // Function(int) get fetchItem => _items.sink.add;
+  // BAD practice, every widget will get their own cache copy
+  // get items => _items.stream.transform(_ItemsTransformer());
+
+  // need to expose items to the outside world only one time
+
+  StoriesBloc() {
+    // does not modify originally stream, a new stream is created and we want it
+    items = _items.stream.transform(_ItemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
