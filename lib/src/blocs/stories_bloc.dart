33a5492c2319724f9ff56fbe6753894044e0f73d
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-07-21 14:37:41 +0200

    fetch item sink method created

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -7,11 +7,14 @@ import '../resources/repository.dart';
 
 class StoriesBloc {
   final _repository = Repository();
-  final _topIds = PublishSubject<List<int>>();
+  final _topIds = PublishSubject<List<int>>(); // Observable of RXDart
+  final _items = BehaviorSubject<int>();
 
   // Getter to Streams
   // https://stackoverflow.com/questions/59336428/observable-is-deprecated-in-rxdart-0-23-1
   Stream<List<int>> get topIds => _topIds.stream;
+
+  Function(int) get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -28,5 +31,6 @@ class StoriesBloc {
 
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
